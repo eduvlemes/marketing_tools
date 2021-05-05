509479fd6c13a701b4154dd11c0e9610f5dfd747
eduvlemes <edu.vlemes@gmail.com>
2021-05-05 13:48:58 -0300

    res set cors

--- functions/routes/alpix/test.js
+++ functions/routes/alpix/test.js
@@ -1,3 +1,10 @@
 exports.get = ({ appSdk }, req, res) => {
+    res.set({
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Methods": "GET, OPTIONS",
+        "Access-Control-Allow-Headers": "Content-Type, Accept-Encoding, Cache-Control",
+        "Access-Control-Max-Age": "600"
+    })
+    
     res.json({msg:"hi"})
 }
