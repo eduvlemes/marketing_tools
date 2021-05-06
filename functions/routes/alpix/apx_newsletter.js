abb626b96175f1e33ca554e48d5668ffb312c3b3
Leonardo Matos <leomp120894@gmail.com>
2021-05-05 21:53:29 -0300

    wip

--- functions/routes/alpix/apx_newsletter.js
+++ functions/routes/alpix/apx_newsletter.js
@@ -1,38 +1,30 @@
-exports.post = ({ appSdk, admin }, req, res) => {
-    const db = admin.firestore()
-    const data = req.body
-    const storeId = data.storeId || 1208
-    
-    if(!db.collection('stores').get(storeId)){
-        db.collection('stores').add({
-            [storeId] : {}
-        })
-        console.log('Adicionou loja ' + storeId)
-    }
+exports.post = async ({ appSdk, admin }, req, res) => {
+  const data = req.body
+  const storeId = data.storeId || 1208
+  const collectionRef = admin.firestore().collection(`s${storeId}_mails`)
 
-    const apx_db = db.collection('stores').get(storeId);
-    
-    const mail = data.mail;
-    const fullname = data.fullname || null;
-    const gender = data.gender || null;
+  const mail = data.mail
+  const fullname = data.fullname || null
+  const gender = data.gender || null
 
-    if(mail != null){
-        const subscriber = {
-            fullname: fullname,
-            mail: mail,
-            gender: gender
-        }
-
-        const query = apx_db.where('mail', '==', mail).get();
-        if(query.empty){
-            apx_db.add(subscriber)
-            console.log('add ' + mail + ' in store ' + storeId)
-            res.send({error: 'false', msg : 'Seu e-mail foi adicionado em nossa lista.'})
-        }
-        console.log('E-mail já inscrito em nossa newsletter.')
-        res.send({error: 'true', msg : 'E-mail já inscrito em nossa newsletter.'})
+  if (mail) {
+    const subscriber = {
+      fullname,
+      mail,
+      gender
     }
     
-    console.log('Nenhuma operação realizada')
-    res.send({error: 'false', msg : 'Nenhuma operação realizada'})
+    const querySnapshot = await collectionRef.where('mail', '==', mail).get()
+    if (querySnapshot.empty) {
+      collectionRef.add(subscriber)
+      console.log('add ' + mail + ' in store ' + storeId)
+      return res.send({error: 'false', msg : 'Seu e-mail foi adicionado em nossa lista.'})
+    }
+    
+    console.log('E-mail já inscrito em nossa newsletter.')
+    return res.send({error: 'true', msg : 'E-mail já inscrito em nossa newsletter.'})
+  }
+    
+  console.log('Nenhuma operação realizada')
+  res.send({error: 'false', msg : 'Nenhuma operação realizada'})
 }
