eee7be9ff4762143f967252bad3486d9417551f2
eduvlemes <edu.vlemes@gmail.com>
2021-05-12 02:43:29 -0300

    envio ok

--- functions/routes/alpix/apx_sendmail.js
+++ functions/routes/alpix/apx_sendmail.js
@@ -5,13 +5,10 @@ exports.post = async ({ appSdk, admin }, req, res) => {
 
   const destination = data.destination
   const subject = data.storeId + ' - ' + data.subject || data.storeId + ' - Sem Assunto'
-  const content = data.content
-  
-  
-  
+  const content = data.content + '<br><br>Enviado utilizando Simple Forms<br>Plugin gratuito temporariamente. Desenvolvido por alpix.dev.'
   
   if (destination != "" && subject != "" && content != "") {
-      collectionRef.add({message: {html: content, subject: subject ,text: content.replace('<br>','\n') }, to: destination })
+      collectionRef.add({message: {html: content, subject: subject , text: content.replace('<br>',' - ') }, to: destination })
       return res.send({error: false, msg : 'Sua mensagem foi enviada.'})
   }else{
     return res.send({error: true, msg : 'Preencha o formulário corretamente ou tente novamente mais tarde.'})
