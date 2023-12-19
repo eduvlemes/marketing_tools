cef44b01611be8098768fb459575058a60daa638
eduvlemes <edu.vlemes@gmail.com>
2023-12-19 17:20:03 -0300

    up

--- functions/routes/alpix/ecomplus_upload.js
+++ functions/routes/alpix/ecomplus_upload.js
@@ -11,46 +11,7 @@ exports.post = async ({ appSdk, admin }, req, res) => {
   const gender = data.gender || null
   console.log(data)
   getAppData({ appSdk, storeId }).then(appData => {
-    //const tryImageUpload = (storeId, auth, originImgUrl, product, index) => new Promise(resolve => {
-      
-        const form = new FormData()
-        form.append('file', Buffer.from(data), originImgUrl.replace(/.*\/([^/]+)$/, '$1'))
-  
-        return axios.post(`https://apx-storage.e-com.plus/${storeId}/api/v1/upload.json`, form, {
-          headers: {
-            ...form.getHeaders(),
-            'X-Store-ID': storeId,
-            'X-My-ID': appData.uploader.x_my_id,
-            'X-Access-Token': appData.uploader.x_access_token
-          }
-        }).then(({ data, status }) => {
-            if (data.picture) {
-              for (const imgSize in data.picture) {
-                if (data.picture[imgSize]) {
-                  if (!data.picture[imgSize].url) {
-                    delete data.picture[imgSize]
-                    continue
-                  }
-                  if (data.picture[imgSize].size !== undefined) {
-                    delete data.picture[imgSize].size
-                  }
-                  data.picture[imgSize].alt = `${product.name} (${imgSize})`
-                }
-              }
-              if (Object.keys(data.picture).length) {
-                return resolve({
-                  _id: ecomUtils.randomObjectId(),
-                  ...data.picture
-                })
-              }
-            }
-            const err = new Error('Unexpected Storage API response')
-            err.response = { data, status }
-            throw err
-        }).catch(err => {
-          console.error(err)
-          res.send(err.message) 
-        })        
+    
         
   }).catch(err => {
     console.error(err)
