12a116faebc41ac2970aef08fb6c7079b0a8fc84
eduvlemes <edu.vlemes@gmail.com>
2023-12-19 16:04:05 -0300

    up

--- functions/routes/alpix/ecomplus_upload.js
+++ functions/routes/alpix/ecomplus_upload.js
@@ -1,32 +1,57 @@
 const getAppData = require('../../lib/store-api/get-app-data')
+const ecomUtils = require('@ecomplus/utils')
+const axios = require('axios')
+const FormData = require('form-data')
+
 exports.post = async ({ appSdk, admin }, req, res) => {
   const storeId = data.storeId
   const data = req.body
   const mail = data.mail
   const fullname = data.fullname || null
   const gender = data.gender || null
+  console.log(data)
   getAppData({ appSdk, storeId }).then(appData => {
-    if (mail) {
-      const mailjet_ = require ('node-mailjet')
-      .connect(appData.mailjet.primary_key, appData.mailjet.primary_secret)
-      const request = mailjet_
-        .post("contact", {'version': 'v3'})
-        .request({
-            "IsExcludedFromCampaigns":"false",
-            "Name": fullname || "",
-            "Email": mail
-          })
-      request
-        .then((result) => {
-          console.log(result.body)
-          res.send({error: false, msg : result.body})
+    //const tryImageUpload = (storeId, auth, originImgUrl, product, index) => new Promise(resolve => {
+      
+        const form = new FormData()
+        form.append('file', Buffer.from(data), originImgUrl.replace(/.*\/([^/]+)$/, '$1'))
+  
+        return axios.post(`https://apx-storage.e-com.plus/${storeId}/api/v1/upload.json`, form, {
+          headers: {
+            ...form.getHeaders(),
+            'X-Store-ID': storeId,
+            'X-My-ID': appData.uploader.x_my_id,
+            'X-Access-Token': appData.uploader.x_access_token
+          }
+        }).then(({ data, status }) => {
+            if (data.picture) {
+              for (const imgSize in data.picture) {
+                if (data.picture[imgSize]) {
+                  if (!data.picture[imgSize].url) {
+                    delete data.picture[imgSize]
+                    continue
+                  }
+                  if (data.picture[imgSize].size !== undefined) {
+                    delete data.picture[imgSize].size
+                  }
+                  data.picture[imgSize].alt = `${product.name} (${imgSize})`
+                }
+              }
+              if (Object.keys(data.picture).length) {
+                return resolve({
+                  _id: ecomUtils.randomObjectId(),
+                  ...data.picture
+                })
+              }
+            }
+            const err = new Error('Unexpected Storage API response')
+            err.response = { data, status }
+            throw err
         })
-        .catch((err) => {
-          console.log(err.statusCode)
-          res.send({error: true, msg : err})
-        })
-    }   
-  })
+      }).catch(err => {
+        console.error(err)
+        res.send(err.message)         
+      })   
   .catch(err => {
     console.error(err)
     res.send(err.message)
